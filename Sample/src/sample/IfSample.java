8194865bc17700384e3357025850a84969a37c0d
dir-FY2025tsujimuraakinori <akinori.tsujimura.t726@dirbato.co.jp>
2025-05-23 15:47:39 +0900

    Chapter6

---
+++ Sample/src/sample/IfSample.java
@@ -0,0 +1,18 @@
+package sample;
+
+public class IfSample {
+
+    public static void main(String[] args) {
+        var score = 80; // 成績
+
+        if (score >= 60) {
+            System.out.println("合格です！");
+        } else if (score >= 80) {
+            System.out.println("よくできました！");
+        } else if (score == 100) {
+            System.out.println("満点です！");
+        } else {
+            System.out.println("赤点です。。。");
+        }
+    }
+}
---
+++ Sample/src/sample/SwitchSample.java
@@ -0,0 +1,24 @@
+package sample;
+
+public class SwitchSample {
+
+    public static void main(String[] args) {
+        int score = 72; // 成績
+
+        switch (score / 10) {
+            case 10:
+                System.out.println("満点です！");
+                break;
+            case 9:
+            case 8:
+                System.out.println("よくできました！");
+                break;
+            case 7:
+            case 6:
+                System.out.println("合格です！");
+                break;
+            default:
+                System.out.println("赤点です。。。");
+        }
+    }
+}
